c5559ad07081c8e7b4e362ff44b942da2e473887
AyushKumar-AK <ayush.am687@gmail.com>
2023-08-13 00:36:54 +0530

    Fixed Background

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "api key here",
+  key: "enter your api key here",
   base: "https://api.openweathermap.org/data/2.5/",
 };
