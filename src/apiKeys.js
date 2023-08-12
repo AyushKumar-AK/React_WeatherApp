7f56838bc3bbc2610947a442087aad5402b7f139
AyushKumar-AK <ayush.am687@gmail.com>
2023-08-12 23:47:40 +0530

    First Commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "api key here",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
